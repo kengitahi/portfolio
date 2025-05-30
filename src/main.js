3a9f780a58215cda4290fefabc9885a1eec30288
Kennedy Gitahi <kengitahi@hotmail.com>
2025-05-30 09:34:55 +0300

    Build for prod

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import '@/assets/css/main.css'
-import { initStickySection } from './assets/js/stickySection'
+import './assets/css/main.css';
+import { initStickySection } from './assets/js/stickySection';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
 
 document.addEventListener('DOMContentLoaded', () => {
-  initStickySection()
-})
+  initStickySection();
+});
